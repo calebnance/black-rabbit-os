f2cce5d9b35a3b3ff6f6ade9036f6a270303e92c
Caleb Nance <calebnance@gmail.com>
2018-09-09 14:39:09 -0400

    no longer needed

--- includes/filehelper.php
+++ includes/filehelper.php
@@ -232,6 +232,8 @@ class FileHelper
 	 	return $string;
  	}
 
+    /*
+    NO LONGER NEEDED
  	public static function br_encrypt($text)
 	{
 		return trim(base64_encode(mcrypt_encrypt(MCRYPT_RIJNDAEL_256, $salt, $text, MCRYPT_MODE_ECB, mcrypt_create_iv(mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_ECB), MCRYPT_RAND))));
@@ -241,6 +243,7 @@ class FileHelper
 	{
 		return trim(mcrypt_decrypt(MCRYPT_RIJNDAEL_256, $salt, base64_decode($text), MCRYPT_MODE_ECB, mcrypt_create_iv(mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_ECB), MCRYPT_RAND)));
 	}
+    */
 
 	public static function startsession($user_info)
 	{
