ac3873628547d7b4eb57a08cdc58dd9cf162fe23
Caleb Nance <calebnance@gmail.com>
2018-09-11 21:17:15 -0400

    format of configuration

--- includes/configuration.php
+++ includes/configuration.php
@@ -1,49 +1,44 @@
 <?php
-/**
- *
- *
- */
+// debug and testing mode
+$live = false;
+$debug = false;
+$create_package = true;
 
-	// debug and testing mode
-	$live = false;
-	$debug = false;
-	$create_package = true;
+define('LIVE', $live);
 
-	define('LIVE', $live);
+// Both
+if(LIVE) {
+	$host = 'localhost';
+	$db_user = '';
+	$db_pass = '';
+	$db_name = '';
+	$email_to	= '';
+	$email_from	= '';
+	$base_url	= 'http://[editthisinconfiguration.php].com/';
 
-	// Both
-	if(LIVE):
-		$host = 'localhost';
-		$db_user = '';
-		$db_pass = '';
-		$db_name = '';
-		$email_to	= '';
-		$email_from	= '';
-		$base_url	= 'http://[editthisinconfiguration.php].com/';
+	// paypal
+	$paypal_url = 'https://www.paypal.com/cgi-bin/webscr';
+	$paypal_btn_id = '';
+	$paypal_pdt_token	= '';
+} else {
+	$host = 'localhost';
+	$db_user = '';
+	$db_pass = '';
+	$db_name = '';
+	$email_to = '';
+	$email_from = '';
+	$base_url	= 'http://black-rabbit-os/';
 
-		// paypal
-		$paypal_url = 'https://www.paypal.com/cgi-bin/webscr';
-		$paypal_btn_id = '';
-		$paypal_pdt_token	= '';
-	else:
-		$host = 'localhost';
-		$db_user = '';
-		$db_pass = '';
-		$db_name = '';
-		$email_to = '';
-		$email_from = '';
-		$base_url	= 'http://black-rabbit-os/';
+	// paypal
+	$paypal_url	= 'https://www.sandbox.paypal.com/cgi-bin/webscr';
+	$paypal_btn_id = '';
+	$paypal_pdt_token	= '';
+}
 
-		// paypal
-		$paypal_url	= 'https://www.sandbox.paypal.com/cgi-bin/webscr';
-		$paypal_btn_id = '';
-		$paypal_pdt_token	= '';
-	endif;
-
-	// download on home page
-	$latesthello25 = 'com_helloworlds-v.1.1.5-joomla_2.5.0.zip';
-	$latesthello30 = 'com_helloworlds-v.1.1.5-joomla_3.0.zip';
-	$latesthello32 = 'com_helloworlds-v.1.1.5-joomla_3.2.zip';
-	$latestallfields25 = 'com_allfields-v.1.1.5-joomla_2.5.0.zip';
-	$latestallfields30 = 'com_allfields-v.1.1.5-joomla_3.0.zip';
-	$latestallfields32 = 'com_allfields-v.1.1.5-joomla_3.2.zip';
+// download on home page
+$latesthello25 = 'com_helloworlds-v.1.1.5-joomla_2.5.0.zip';
+$latesthello30 = 'com_helloworlds-v.1.1.5-joomla_3.0.zip';
+$latesthello32 = 'com_helloworlds-v.1.1.5-joomla_3.2.zip';
+$latestallfields25 = 'com_allfields-v.1.1.5-joomla_2.5.0.zip';
+$latestallfields30 = 'com_allfields-v.1.1.5-joomla_3.0.zip';
+$latestallfields32 = 'com_allfields-v.1.1.5-joomla_3.2.zip';
