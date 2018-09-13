8f4626784bde2c624d9c0b112b5fa8ea9a6e805e
Caleb Nance <calebnance@gmail.com>
2018-09-13 09:30:23 -0400

    start of bootstrap 4 support?

--- admin/login.php
+++ admin/login.php
@@ -56,36 +56,29 @@ if(isset($_REQUEST['msg'])){
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta name="author" content="Caleb Nance - www.calebnance.com">
 		<!-- Le styles -->
-		<link href="../bootstrap/css/bootstrap.css" rel="stylesheet">
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 		<style type="text/css">
 			body { padding-top: 40px; padding-bottom: 40px; background-color: #f5f5f5;}
-			.row { margin: 30px 0 0 0; }
-			.form-signin { max-width: 300px; padding: 19px 29px 29px; margin: 0 auto 20px; background-color: #fff; border: 1px solid #e5e5e5; -webkit-border-radius: 5px; -moz-border-radius: 5px; border-radius: 5px; -webkit-box-shadow: 0 1px 2px rgba(0,0,0,.05); -moz-box-shadow: 0 1px 2px rgba(0,0,0,.05); box-shadow: 0 1px 2px rgba(0,0,0,.05); }
+			.form-signin { max-width: 320px; padding: 19px 29px 29px; margin: 0 auto 20px; background-color: #fff; border: 1px solid #e5e5e5; -webkit-border-radius: 5px; -moz-border-radius: 5px; border-radius: 5px; -webkit-box-shadow: 0 1px 2px rgba(0,0,0,.05); -moz-box-shadow: 0 1px 2px rgba(0,0,0,.05); box-shadow: 0 1px 2px rgba(0,0,0,.05); }
 			.form-signin .form-signin-heading, .form-signin .checkbox { margin-bottom: 10px; }
 			.form-signin input[type="text"],.form-signin input[type="password"] { font-size: 16px; height: auto; margin-bottom: 15px; padding: 7px 9px; }
+			.btn { display: block; width: 120px; }
 		</style>
-		<link href="../bootstrap/css/bootstrap-responsive.css" rel="stylesheet">
-		<!--[if lt IE 9]>
-		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
-		<![endif]-->
 	</head>
 	<body>
 		<div class="container">
-			<div class="row">
-				<form class="form-signin" method="post" action="login.php">
-					<h2 class="form-signin-heading">Admin Sign In</h2>
-					<?php
-					Msg::alert($msg, $msgType);
-					?>
-					<input type="text" name="username" class="input-block-level" placeholder="Username">
-					<input type="password" name="password" class="input-block-level" placeholder="Password">
-					<button class="btn btn-large btn-success" type="submit">Sign in</button>
-				</form>
-			</div>
+			<form class="form-signin" method="post" action="login.php">
+				<h2 class="form-signin-heading">Admin Sign In</h2>
+				<?php
+				Msg::alert($msg, $msgType);
+				?>
+				<input type="text" name="username" class="input-block-level" placeholder="Username">
+				<input type="password" name="password" class="input-block-level" placeholder="Password">
+				<button class="btn btn-large btn-success mx-auto" type="submit">Sign in</button>
+			</form>
 		</div>
-		<!-- Le javascript
-		================================================== -->
-		<script src="../js/jquery-1.8.3.min.js"></script>
-		<script src="../bootstrap/js/bootstrap.min.js"></script>
+		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 	</body>
 </html>
